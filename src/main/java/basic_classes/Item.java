300fe1fac2c21bbde69a39f9097e954353acee8c
Mo'men Waleed <momen.waleed306@gmail.com>
2022-06-28 01:38:18 +0200

    Added search by item & search by category

--- src/main/java/basic_classes/Item.java
+++ src/main/java/basic_classes/Item.java
@@ -1,5 +1,9 @@
 package basic_classes;
+import database.manager.DatabaseManager;
+
 import java.sql.*;
+import java.util.Locale;
+import java.util.Vector;
 
 public class Item {
     private static final boolean debug = true;
@@ -8,12 +12,12 @@ public class Item {
     private double price;
     private String item_name;
     private String seller_name;
-    private double stock;
+    private int stock;
     private String category;
 
     // Constructors
 
-    public Item(int iid, double price, String item_name, String seller_name, double stock, String category){
+    public Item(int iid, double price, String item_name, String seller_name, int stock, String category){
         this.iid = iid;
         this.price = price;
         this.stock = stock;
@@ -28,7 +32,7 @@ public class Item {
     public double getPrice(){
         return price;
     }
-    public double getStock(){
+    public int getStock(){
         return stock;
     }
     public String getItem_name(){
@@ -75,7 +79,7 @@ public class Item {
     public void addItem(Connection conn){
         try{
             String query;
-            query  = "insert into ITEM values (" + iid + "," + price + "," + insertQuotations(item_name)+ "," + insertQuotations(seller_name) + "," + stock + "," + insertQuotations(category) + ")";
+            query  = "insert into ITEM(iid, price, item_name, seller_name, stock, category) values (" + iid + "," + price + "," + insertQuotations(item_name)+ "," + insertQuotations(seller_name) + "," + stock + "," + insertQuotations(category) + ")";
             Statement stmt = conn.createStatement();
             stmt.executeUpdate(query);
         }
@@ -97,7 +101,7 @@ public class Item {
             Statement stmt = conn.createStatement();
             ResultSet queryResult = stmt.executeQuery(query);
             queryResult.next();
-            return new Item(queryResult.getInt(queryResult.findColumn("IID")) , queryResult.getFloat(queryResult.findColumn("PRICE")) , queryResult.getString(queryResult.findColumn("ITEM_NAME")) , queryResult.getString(queryResult.findColumn("SELLER_NAME")) , queryResult.getDouble(queryResult.findColumn("STOCK")) , queryResult.getString(queryResult.findColumn("CATEGORY")));
+            return new Item(queryResult.getInt(queryResult.findColumn("IID")) , queryResult.getFloat(queryResult.findColumn("PRICE")) , queryResult.getString(queryResult.findColumn("ITEM_NAME")) , queryResult.getString(queryResult.findColumn("SELLER_NAME")) , queryResult.getInt(queryResult.findColumn("STOCK")) , queryResult.getString(queryResult.findColumn("CATEGORY")));
         }
         catch(SQLException e){
             if(debug){
@@ -133,13 +137,13 @@ public class Item {
      * PRE_CONDITIONS: item_name exists in the database
      * POST_CONDITIONS: retrieves item stock
      */
-    public static double getStock(String item_name, Connection conn) {
+    public static int getStock(String item_name, Connection conn) {
         try {
             String query = "select * from ITEM where ITEM_NAME = " + insertQuotations(item_name);
             Statement stmt = conn.createStatement();
             ResultSet queryResult = stmt.executeQuery(query);
             queryResult.next();
-            return (queryResult.getDouble(queryResult.findColumn("STOCK")));
+            return (queryResult.getInt(queryResult.findColumn("STOCK")));
         } catch (SQLException e) {
             if (debug) {
                 System.out.println("SQLException: " + e.getMessage());
@@ -172,9 +176,59 @@ public class Item {
         return"Not Found";
     }
 
+    public static Vector<Item> search_by_category(String category, Connection conn){
+        try {
+            Vector<Item> items = new Vector<Item>();
+            String query = "select * from ITEM where CATEGORY = " + insertQuotations(category);
+            Statement stmt = conn.createStatement();
+            ResultSet queryResult = stmt.executeQuery(query);
+            while(queryResult.next()) {
+                items.add(new Item(queryResult.getInt(queryResult.findColumn("IID")), queryResult.getFloat(queryResult.findColumn("PRICE")), queryResult.getString(queryResult.findColumn("ITEM_NAME")), queryResult.getString(queryResult.findColumn("SELLER_NAME")), queryResult.getInt(queryResult.findColumn("STOCK")), queryResult.getString(queryResult.findColumn("CATEGORY"))));
+            }
+            return items;
+        } catch (SQLException e) {
+            if (debug) {
+                System.out.println("SQLException: " + e.getMessage());
+                System.out.println("SQLState: " + e.getSQLState());
+            }
+        }
+        return null;
+    }
+
+    public static Vector<Item> search_by_name(String item_name, Connection conn){
+        try {
+            Vector<Item> items = new Vector<Item>();
+            String query = "select * from ITEM where ITEM_NAME = " + insertQuotations(item_name);
+            Statement stmt = conn.createStatement();
+            ResultSet queryResult = stmt.executeQuery(query);
+            while(queryResult.next()) {
+                items.add(new Item(queryResult.getInt(queryResult.findColumn("IID")), queryResult.getFloat(queryResult.findColumn("PRICE")), queryResult.getString(queryResult.findColumn("ITEM_NAME")), queryResult.getString(queryResult.findColumn("SELLER_NAME")), queryResult.getInt(queryResult.findColumn("STOCK")), queryResult.getString(queryResult.findColumn("CATEGORY"))));
+            }
+            return items;
+        } catch (SQLException e) {
+            if (debug) {
+                System.out.println("SQLException: " + e.getMessage());
+                System.out.println("SQLState: " + e.getSQLState());
+            }
+        }
+        return null;
+    }
 
     private static String insertQuotations(String str){
         return "\"" + str + "\"" ;
     }
 
+    @Override
+    public String toString(){
+        return "iid: " + iid + "\nprice: " + price + "\nitem_name: " + item_name + "\nseller_name: " + seller_name + "\nstock: " + stock + "\ncategory: " + category;
+    }
+    public static void main(String[] args) {
+        DatabaseManager.initConnection(10);
+        Connection conn = DatabaseManager.requestConnection();
+        Vector<Item> i1 = search_by_name("cats", conn);
+        for (Item i:i1
+             ) {
+            System.out.println(i);
+        }
+    }
 }
