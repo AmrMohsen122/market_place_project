34f2bf31c45e56b1e6bc29eec36fef8d148ac1ae
amrma <amrmah785@gmail.com>
2022-06-29 01:03:35 +0200

    added required in method

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module parallelProject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.logging;
 
     opens parallelProject to javafx.fxml;
     exports parallelProject;
