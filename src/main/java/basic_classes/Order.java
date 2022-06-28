78f3b26c31dfbe43189a65a424ea874a3a1eacf6
Mo'men Waleed <momen.waleed306@gmail.com>
2022-06-28 02:24:08 +0200

    Added LoadItems

--- src/main/java/basic_classes/Order.java
+++ src/main/java/basic_classes/Order.java
@@ -1,11 +1,19 @@
 package basic_classes;
 
-import java.sql.Date;
+import java.sql.*;
+import java.util.Vector;
 
 public class Order {
     private int OID;
+    protected static final boolean debug = true;
     private Date ODate;
     private double totalPrice;
+
+    public Vector<Item> getItems() {
+        return items;
+    }
+
+    private Vector<Item> items = new Vector<Item>();
 
     public Order(int OID, Date ODate, double totalPrice) {
         this.OID = OID;
@@ -30,6 +38,23 @@ public class Order {
 
     public double getTotalPrice() {
         return totalPrice;
+    }
+
+    public void loadItems(Connection conn){
+        try{
+            String query = "select IID , PRICE, ITEM_NAME, SELLER_NAME, STOCK, CATEGORY from ITEM natural join CUST_ORDER where OID = " + OID;
+            Statement stmt = conn.createStatement();
+            ResultSet queryResult = stmt.executeQuery(query);
+            while(queryResult.next()){
+                items.add(new Item(queryResult.getInt(queryResult.findColumn("IID")) , queryResult.getDouble(queryResult.findColumn("PRICE"))
+                        ,queryResult.getString(queryResult.findColumn("ITEM_NAME")), queryResult.getString(queryResult.findColumn("SELLER_NAME")), queryResult.getInt(queryResult.findColumn("STOCK")), queryResult.getString(queryResult.findColumn("CATEGORY"))));
+            }
+        }catch(SQLException e){
+            if(debug){
+                System.out.println("SQLException: " + e.getMessage());
+                System.out.println("SQLState: " + e.getSQLState());
+            }
+        }
     }
 
     @Override
