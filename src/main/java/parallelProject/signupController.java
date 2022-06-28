04b9f798c00c0cdd19d22776118c7762eae477c2
bassanttsayed <85302147+bassanttsayed@users.noreply.github.com>
2022-06-28 02:32:33 +0200

    Add files via upload

---
+++ src/main/java/parallelProject/signupController.java
@@ -0,0 +1,25 @@
+package parallelProject;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class signupController {
+    @FXML
+    public void gologin(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Home Page");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
