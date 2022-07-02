9fe330554d2195680a6df7961f87fbcd82bc537f
bassanttsayed <85302147+bassanttsayed@users.noreply.github.com>
2022-07-02 02:39:55 +0200

    Update transactionController.java

--- src/main/java/parallelProject/transactionController.java
+++ src/main/java/parallelProject/transactionController.java
@@ -24,16 +24,14 @@ import static parallelProject.adminMenuController.allUsers;
 
 
 public class transactionController implements Initializable {
-    public static TreeItem<String> ORDER;
-    public static TreeItem<String> branch1;
+
 
     @FXML
     private TreeView transactionTree;
 
     @FXML
     public void goback(ActionEvent event) throws IOException {
-        System.out.println(adminViewUsersController.indexUser);
-        ORDER.getChildren().removeAll(branch1);
+        //ORDER.getChildren().removeAll(branch1);
         Parent root = FXMLLoader.load(getClass().getResource("adminViewUsers.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setTitle("All Users");
@@ -45,29 +43,39 @@ public class transactionController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ORDER = new TreeItem<>("Orders:");
-        transactionTree.setRoot(ORDER);
+        TreeItem<String> Users = new TreeItem<>("All Users:");
+        transactionTree.setRoot(Users);
+        String branchU = "User ";
         String branchO = "Order ";
         String branchI = "item ";
 
-        Customer showCustomerTransaction = allUsers.get(adminViewUsersController.indexUser); //TODO indexUser instead of 0
-        for(int o=0 ; o<showCustomerTransaction.getOrders().size() ; o++){
-            branch1 = new TreeItem<>(branchO+ (Integer.toString(o+1)));
-            ORDER.getChildren().addAll(branch1);
-            String test1 = Double.toString(showCustomerTransaction.getOrders().get(o).getTotalPrice());
-            // TreeItem<String> branch11 = new TreeItem<>("Order Date: " + test2);
-            TreeItem<String> branch12 = new TreeItem<>("Order Price: " +test1);
-            TreeItem<String> branch13 = new TreeItem<>("Items: ");
-            branch1.getChildren().addAll(branch12,branch13);
+        for (int k = 0; k < allUsers.size(); k++) {
+            TreeItem<String> branchU1 = new TreeItem<>(branchU + (Integer.toString(k + 1) + "  " + (allUsers.get(k).getUsername()).toString()));
+            Users.getChildren().addAll(branchU1);
 
-            for(int m=0 ; m<showCustomerTransaction.getOrders().get(o).getItems().size() ; m++){
-                TreeItem<String> branch131 = new TreeItem<>(branchI+ (Integer.toString(m+1)));
-                branch13.getChildren().addAll(branch131);
-                TreeItem<String> branch1311 = new TreeItem<>("Item #: " + Integer.toString(showCustomerTransaction.getOrders().get(o).getItems().get(m).getIid()));
-                TreeItem<String> branch1312 = new TreeItem<>("Item Price: " + Double.toString(showCustomerTransaction.getOrders().get(o).getItems().get(m).getPrice()));
-                TreeItem<String> branch1313 = new TreeItem<>("Item Name: " + showCustomerTransaction.getOrders().get(o).getItems().get(m).getItem_name());
-                TreeItem<String> branch1314 = new TreeItem<>("Item Quantity: " + Integer.toString(showCustomerTransaction.getOrders().get(o).getItems().get(m).getItemQuantity()));
-                branch131.getChildren().addAll(branch1311,branch1312,branch1313,branch1314);
+            TreeItem<String> ORDER = new TreeItem<>("Orders:");
+            Customer showCustomerTransaction = allUsers.get(k);
+            branchU1.getChildren().addAll(ORDER);
+
+            for (int o = 0; o < showCustomerTransaction.getOrders().size(); o++) {
+                TreeItem<String> branch1 = new TreeItem<>(branchO + (Integer.toString(o + 1)));
+                ORDER.getChildren().addAll(branch1);
+
+                String test1 = Double.toString(showCustomerTransaction.getOrders().get(o).getTotalPrice());
+                // TreeItem<String> branch11 = new TreeItem<>("Order Date: " + test2);
+                TreeItem<String> branch12 = new TreeItem<>("Order Price: " + test1);
+                TreeItem<String> branch13 = new TreeItem<>("Items: ");
+                branch1.getChildren().addAll(branch12, branch13);
+
+                for (int m = 0; m < showCustomerTransaction.getOrders().get(o).getItems().size(); m++) {
+                    TreeItem<String> branch131 = new TreeItem<>(branchI + (Integer.toString(m + 1)));
+                    branch13.getChildren().addAll(branch131);
+                    TreeItem<String> branch1311 = new TreeItem<>("Item #: " + Integer.toString(showCustomerTransaction.getOrders().get(o).getItems().get(m).getIid()));
+                    TreeItem<String> branch1312 = new TreeItem<>("Item Price: " + Double.toString(showCustomerTransaction.getOrders().get(o).getItems().get(m).getPrice()));
+                    TreeItem<String> branch1313 = new TreeItem<>("Item Name: " + showCustomerTransaction.getOrders().get(o).getItems().get(m).getItem_name());
+                    TreeItem<String> branch1314 = new TreeItem<>("Item Quantity: " + Integer.toString(showCustomerTransaction.getOrders().get(o).getItems().get(m).getItemQuantity()));
+                    branch131.getChildren().addAll(branch1311, branch1312, branch1313, branch1314);
+                }
             }
         }
     }
